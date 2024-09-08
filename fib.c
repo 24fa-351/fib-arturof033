99f324f97142cd742b76c523a122fd161d115e27
arturof033 <68297760+arturof033@users.noreply.github.com>
2024-09-07 23:12:08 -0700

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   printf("Hello!\n");
+#define MAX_LEN 10
+
+int iterativeFibonacci(int n)
+{
+    int value1 = 0;
+    int value2 = 1;
+    int result = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        result = value1 + value2;
+        value2 = value1;
+        value1 = result; 
+    }
+
+    return result;
+}
+
+int recursiveFibonacci(int n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+int main(int argc, char * argv[]) {
+
+    char num[MAX_LEN];
+
+    FILE *fptr;
+    fptr =  fopen(argv[3], "r");
+    fgets (num, MAX_LEN, fptr);
+
+    int N = atoi(num) + atoi(argv[1]) - 1;
+
+    if (*argv[2] == 'i')
+    {
+        printf("%d", iterativeFibonacci(N));
+    }
+
+    if (*argv[2] == 'r')
+    {
+        printf("%d", recursiveFibonacci(N));
+    }
+
    return 0;
 }
